022c78202b645acfbb85fa13e7c9a0d83635d9a7
dominikusbrian <domi@dominikusbrian.com>
2024-06-02 22:37:19 +0800

    Updateing the API and Code Formatting for AI-Agents

--- app/(dashboard)/(routes)/code/page.tsx
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -3,7 +3,7 @@
 // Public Global here
 import * as z from "zod";
 import { zodResolver } from "@hookform/resolvers/zod"; // Control Validation for form
-import { MessageSquare } from "lucide-react";
+import { Code } from "lucide-react";
 import { useForm } from "react-hook-form";
 import { Form, FormField, FormItem, FormControl, FormLabel } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -12,6 +12,7 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import ReactMarkdown from "react-markdown";
 
 // Local here
 import { Heading } from "@/components/heading";
@@ -22,10 +23,9 @@ import { cn } from "@/lib/utils";
 import { UserAvatar } from "@/components/user-avatar";
 import { BotAvatar } from "@/components/bot-avatar";
 
-const ChatPage = () => {
+const CodePage = () => {
     const router = useRouter();
     const [messages, setMessages] = useState<ChatCompletionMessageParam[]>([]);
-    
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -50,12 +50,10 @@ const ChatPage = () => {
             };
             const newMessages = [...messages, userMessage];
 
-            const response = await axios.post("/api/chat", {
+            const response = await axios.post("/api/code", {
                 messages: newMessages,
                 model: values.llm_engine,
             });
-
-            
 
             setMessages((current) => [...current, userMessage, response.data]);
             form.reset({ prompt: "", llm_engine: values.llm_engine }); // Preserve selected LLM engine
@@ -63,7 +61,7 @@ const ChatPage = () => {
             if (error.code === 'ETIMEDOUT') {
                 console.log('Request timed out. Please try again.');
             } else {
-                console.error("CHAT_ERROR", error);
+                console.error("CODE_ERROR", error);
             }
         } finally {
             router.refresh();
@@ -73,11 +71,11 @@ const ChatPage = () => {
     return (
         <div>
             <Heading
-                title="PhDBot Chat Interface"
-                description="Your PhDBot live here, train and talk through here."
-                icon={MessageSquare}
-                iconColor="text-violet-500"
-                bgColor="bg-violet-500/10"
+                title="PhDBot Code Generation"
+                description="PhDBot can help you with code for analysis and automation."
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
             />
             <div className="px-4 lg:px-8">
                 <div>
@@ -90,7 +88,7 @@ const ChatPage = () => {
                                 name="llm_engine"
                                 render={({ field }) => (
                                     <FormItem className="col-span-12 lg:col-span-12 w-full">
-                                        <FormLabel className="font-bold">LLM Engine</FormLabel>
+                                        <FormLabel>LLM Engine</FormLabel>
                                         <FormControl className="m-0 p-0">
                                             <select
                                                 className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
@@ -115,12 +113,12 @@ const ChatPage = () => {
                                 name="prompt"
                                 render={({ field }) => (
                                     <FormItem className="col-span-12 mt-4 ">
-                                        <FormLabel className="font-bold" >Prompt</FormLabel>
+                                        <FormLabel>Prompt</FormLabel>
                                         <FormControl className="m-0 p-0">
                                             <Input
                                                 className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
                                                 disabled={isLoading}
-                                                placeholder="Your Question: e.g. What's the latest breakthrough in Green Computing?"
+                                                placeholder="Decribe what you want the code to perform."
                                                 {...field}
                                             />
                                         </FormControl>
@@ -144,8 +142,8 @@ const ChatPage = () => {
                     )}
 
                     {messages.length === 0 && !isLoading && (
-                        <div>
-                            <Empty label="No Discussion yet with PhDBot by DreamBrook Labs" />
+                        <div className = "mt-4">
+                            <Empty  label="No Discussion yet with PhDBot by DreamBrook Labs" />
                         </div>
                     )}
                     <div className="flex flex-col-reverse gap-y-4">
@@ -157,9 +155,21 @@ const ChatPage = () => {
                                 )}
                             >
                                 {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
-                                <div className="text-sm">
-                                    {message.content}
-                                </div>
+                                <ReactMarkdown
+                                components={{
+                                    pre: ({node, ...props}) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props}/>
+                                        </div>
+                                    ),
+                                    code: ({node, ...props}) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props}/>
+                                    ),
+                                }}
+                                className="text-sm overflow-hidden leading-7"
+                                >
+                                    {message.content || ""}
+                                </ReactMarkdown>
                             </div>
                         ))}
                     </div>
@@ -169,4 +179,4 @@ const ChatPage = () => {
     );
 }
 
-export default ChatPage;
+export default CodePage;
--- app/api/code/route.ts
+++ app/api/code/route.ts
@@ -4,46 +4,12 @@ import OpenAI from "openai";
 import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 
 
-// Repositories of Instruction messages 
-// TO-DO in the future this suppose to be modifiable by user, or can be uploaded
-const OpenAI_instructionMessage: ChatCompletionMessageParam = {
+const instructionMessage: ChatCompletionMessageParam = {
     role: "system",
-    content: `You're OpenAI AI-Assistant. You are using gpt-4o as your LLM engine. 
-    You are helpful and knowledgable about most things in general.
-    You are not opiniated in your view and can be creative when helping users answers their questions, or resolving issues.`
+    content: "You are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
 }
-
-
-const AkashChat_instructionMessage: ChatCompletionMessageParam = {
-    role: "system",
-    content: `You're AkashChat. You are using llama3-8b as your LLM engine. You are helpful and knowledgable about akash network. 
-    Akash is an open network that lets users buy and sell computing resources securely and efficiently. 
-    Purpose-built for public utility. When user say you're not knowledgable enough, 
-    be humble and state your knowledge base is still under development`
-}
-
-
-const PhDBot_instructionMessage: ChatCompletionMessageParam = {
-    role: "system",
-    content: `You're PhDBot. A PhD-Level Personalized AI-Agents developed by DreamBrook Labs. 
-    You're still under development so are not working as good as it may become. But you're very smart and intelligible in subject matter 
-    being discussed without ever being too obsessed with detail unless user ask for it. You will have access to specific domain knowledge, 
-    though some of this action tools are not available just yet.`
-}
-
-
-const Ollama_instructionMessage: ChatCompletionMessageParam = {
-    role: "system",
-    content: `You're Ollama powered AI Agent. You can be using any of the LLM-Engine accessible through the ollama server.
-    This also means you can have access to all other Hugging Face model by nature. You are helpful and humble in your language,
-    polite and answer questions in precise but friendly manner `
-}
-let instructionMessage;
-
 
 export async function POST(req: Request) {
-
-
     try {
         const { userId } = auth();
         const body = await req.json();
@@ -67,7 +33,6 @@ export async function POST(req: Request) {
                     baseURL: process.env.AKASH_BASEURL,
                 });
                 modelToUse = "llama3-8b";
-                instructionMessage = AkashChat_instructionMessage;
                 break;
             case "OpenAI":
                 openai = new OpenAI({
@@ -75,7 +40,6 @@ export async function POST(req: Request) {
                     baseURL: process.env.FORWARDED_OPENAI_URL, // Adjust if necessary
                 });
                 modelToUse = "gpt-4o";
-                instructionMessage = OpenAI_instructionMessage;
                 break;
             case "PhDBot":
                 openai = new OpenAI({
@@ -83,7 +47,6 @@ export async function POST(req: Request) {
                     baseURL: process.env.PHDBOT_BASEURL
                 });
                 modelToUse = "llama3-8b";
-                instructionMessage = PhDBot_instructionMessage;
                 break;
             case "Ollama":
                 openai = new OpenAI({
@@ -91,7 +54,6 @@ export async function POST(req: Request) {
                     baseURL: process.env.OLLAMA_BASEURL, // Replace with the actual endpoint
                 });
                 modelToUse = "llama3-8b";
-                instructionMessage = Ollama_instructionMessage;
                 break;
             default:
                 return new NextResponse("Unsupported model specified", { status: 400 });
@@ -103,11 +65,11 @@ export async function POST(req: Request) {
         });
 
         // Log the response for debugging
-        console.log("Chat response:", response.choices[0].message);
+        console.log("Code response:", response.choices[0].message);
 
         return NextResponse.json(response.choices[0].message, { status: 200 });
     } catch (error) {
-        console.log("CHAT_ERROR", error);
+        console.log("CODE_ERROR", error);
         return new NextResponse("Internal Error", { status: 500 });
     }
 }
